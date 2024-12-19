bae626fb99cb1e9bdc549e3e2d25a5206a685c11
Yogendra <a.yogendra65@gmail.com>
2024-12-19 08:15:03 +0530

    Updated Catalog

--- main.js
+++ main.js
@@ -1,12 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
 
 const mainRoot = document.getElementById("root");
 const mainDom = ReactDOM.createRoot(mainRoot);
 
 const LogoComponent = function () {
-    return <a href="#"><img src="https://img.freepik.com/premium-vector/phone-logo-smartphone-icon_701361-965.jpg" alt="Logo" className="logo" /></a>;
+    return <a href="#"><img src="https://img.freepik.com/premium-vector/phone-logo-smartphone-icon_701361-965.jpg" alt="Logo" className="logo" /></a>
 };
 
 const MenuComponent = function () {
@@ -14,7 +13,7 @@ const MenuComponent = function () {
         <nav>
             <ul className="menu">
                 <li><a href="#home">Home</a></li>
-                <li><a href="#about">About Us</a></li>
+                <li><a href="#about">About</a></li>
                 <li><a href="#contact">Contact</a></li>
             </ul>
         </nav>
